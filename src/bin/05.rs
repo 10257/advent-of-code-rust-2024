522805a697f0beedb1223a549b22b7a4d0e62be8
jgreau <jgreau@student.42.fr>
2024-12-07 05:31:14 +0100

    Day 5 part 2 solution idea for further optimization: use the part 2 intersection check to find if a solution is correct or not inteqd of the check slice function.

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -15,7 +15,7 @@ pub fn check_slice(rule_set: Option<&FxHashSet<u8>>, page_list: &[u8], index: &u
     }
 
     let page_remain_hash = FxHashSet::from_iter(page_remain.iter().copied());
-    // println!("rule{:?} - remain{:?}", rule_set, page_remain_hash);
+    // println!("rule:{:?} - remain:{:?}", rule_set, page_remain_hash);
     if !rule_set
         .unwrap_or(&FxHashSet::default())
         .is_superset(&page_remain_hash)
@@ -23,7 +23,7 @@ pub fn check_slice(rule_set: Option<&FxHashSet<u8>>, page_list: &[u8], index: &u
         return false;
     }
     let page_done_hash = FxHashSet::from_iter(page_list[..*index].iter().copied());
-    // println!("rule{:?} - done{:?}", rule_set, page_done_hash);
+    // println!("rule:{:?} - done:{:?}", rule_set, page_done_hash);
     if !page_done_hash.is_disjoint(&rule_set.unwrap_or(&FxHashSet::default())) {
         return false;
     }
@@ -52,15 +52,59 @@ pub fn part_one(input: &str) -> Option<u32> {
             check_slice(rules.get(page_num), &page_list, &nb)
         }) {
             // eprintln!("{} {}", acc, page_list[page_list.len() / 2]);
-            return acc + page_list[page_list.len() / 2];
+            return acc + page_list[page_list.len() / 2] as u32;
         }
         acc
     });
-    Some(sum_result as u32)
+    Some(sum_result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut rules: FxHashMap<u8, FxHashSet<u8>> = FxHashMap::default();
+    let (rules_str, to_print_str) = input.split("\n\n").next_tuple().unwrap();
+    for line in rules_str.lines() {
+        let (page_n1, page_n2) = line
+            .split('|')
+            .map(|nb| nb.parse().unwrap())
+            .next_tuple()
+            .unwrap();
+        rules.entry(page_n1).or_default().insert(page_n2);
+    }
+    let sum_result = to_print_str.lines().filter_map(|line| {
+        let values = line.split(',');
+        let mut page_list: SmallVec<[u8; 24]> = smallvec![];
+        for val in values {
+            page_list.push(val.parse().unwrap());
+        }
+        if !page_list.iter().enumerate().all(|(nb, page_num)| {
+            // eprintln!("len {:?} - i {:?}", page_list.len(), nb);
+            check_slice(rules.get(page_num), &page_list, &nb)
+        })
+        {
+            return Some(page_list);
+        }
+        None
+    }).fold(0, |acc, wrong_list| {
+        let mut order_list: SmallVec<[(u8, u8); 24]> = smallvec![];
+
+        let page_list_hash = FxHashSet::from_iter(wrong_list.iter().copied());
+        for page_nb in wrong_list {
+            let common = rules.get(&page_nb)
+                .unwrap_or(&FxHashSet::default()).intersection(&page_list_hash).count() as u8;
+            order_list.push((common, page_nb));
+        }
+        order_list.sort_unstable();
+        order_list.reverse();
+        let (_, pages_new): (SmallVec<[u8; 24]>, SmallVec<[u8; 24]>) = order_list.iter().copied().unzip();
+        if pages_new.iter().enumerate().all(|(nb, page_num)| {
+            check_slice(rules.get(page_num), &pages_new, &nb)
+        })
+        {
+            return acc + pages_new[pages_new.len() / 2] as u32;
+        }
+        acc
+    });
+    Some(sum_result)
 }
 
 #[cfg(test)]
@@ -76,6 +120,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
