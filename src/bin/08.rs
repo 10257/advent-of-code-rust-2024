01238aade389821f9da9a3757a38f9021edff1f2
jgreau <jgreau@student.42.fr>
2024-12-11 02:28:43 +0100

    day 8 rewrite using closure to limit copy paste of code

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -42,19 +42,31 @@ pub fn make_antenna_db(input: &str) -> (FxHashMap<u8, Vec<Point>>, usize) {
     (antennaes, width)
 }
 
+pub fn find_antinodes<F>(antennaes: FxHashMap<u8, Vec<Point>>, mut anti_f: F)
+    where
+     F: FnMut(&Point, &Point)
+{
+    antennaes.iter().for_each(|(_, positions)| {
+        positions.iter().for_each(|pos|{
+            positions.iter().for_each(|point| {
+                anti_f(point, pos);
+            });
+        });
+    });
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let (antennaes, width) = make_antenna_db(input);
     // println!("{:#?}", antennaes);
     let mut antinodes: FxHashSet<Point> = FxHashSet::default();
-    antennaes.iter().for_each(|(_, positions)| {
-        positions.iter().for_each(|pos|{
-            positions.iter().filter(|&point| point != pos).for_each(|point| {
-                if let Some(anti) = point.antinodes(pos, &width) {
-                    // println!("{:?}", anti);
-                    antinodes.insert(anti);
-                }
-            });
-        });
+    find_antinodes(antennaes, |point, center|{
+        if point == center {
+            return;
+        }
+        if let Some(anti) = point.antinodes(center, &width) {
+            // println!("{:?}", anti);
+            antinodes.insert(anti);
+        }
     });
     Some(antinodes.len() as u32)
 }
@@ -62,20 +74,19 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let (antennaes, width) = make_antenna_db(input);
     let mut antinodes: FxHashSet<Point> = FxHashSet::default();
-    antennaes.iter().for_each(|(_, positions)| {
-        positions.iter().for_each(|pos|{
-            antinodes.insert(*pos);
-            positions.iter().filter(|&point| point != pos).for_each(|point| {
-                let mut center = *pos;
-                let mut point_cpy = *point;
-                while let Some(anti) = point_cpy.antinodes(&center, &width) {
-                    // println!("{:?}", anti);
-                    antinodes.insert(anti);
-                    point_cpy = center;
-                    center = anti;
-                }
-            });
-        });
+    find_antinodes(antennaes, |point, center|{
+        if point == center {
+            antinodes.insert(*point);
+            return;
+        }
+        let mut center_cpy = *center;
+        let mut point_cpy = *point;
+        while let Some(anti) = point_cpy.antinodes(&center_cpy, &width) {
+            // println!("{:?}", anti);
+            antinodes.insert(anti);
+            point_cpy = center_cpy;
+            center_cpy = anti;
+        }
     });
     Some(antinodes.len() as u32)
 }
