9f18190603e890e9eeb7c3ef3ccac634141bc4ab
jgreau <jgreau@student.42.fr>
2024-12-24 14:43:45 +0100

    day 11 tried another naive approach for part 2 but still not working

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -33,35 +33,44 @@ pub fn create_stones<const N: usize>(input: &str) -> Option<u32> {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    // let mut stones: Vec<u64> = input.split_ascii_whitespace().map(|stone| stone.parse().unwrap()).collect_vec();
-    // let mut new_stones: Vec<u64> = vec![];
-
-    // for _ in 0..25 {
-    //     stones.iter().for_each(|&stone| {
-    //         if stone == 0 {
-    //             new_stones.push(1);
-    //             return;
-    //         }
-    //         let len = stone.ilog10() + 1;
-    //         match len % 2 {
-    //             0 => {
-    //                 new_stones.push(stone.div_euclid(10u64.pow(len/2)));
-    //                 new_stones.push(stone.rem_euclid(10u64.pow(len/2)));
-    //             },
-    //             1 => new_stones.push(stone * 2024),
-    //             _ => {}
-    //         }
-    //     });
-    //     stones = new_stones.clone();
-    //     new_stones.clear();
-    // }
-    // Some(stones.len() as u32)
     create_stones::<25>(input)
+}
 
+pub fn create_stone_alt(stones_src: &mut [u64]) -> Option<u32> {
+    let mut stones: Vec<u64> = vec![1];
+    stones.clone_from_slice(stones_src);
+    let mut new_stones: Vec<u64> = vec![];
+    for _ in 0..75 {
+        stones.iter().for_each(|&stone| {
+            if stone == 0 {
+                new_stones.push(1);
+                return;
+            }
+            let len = stone.ilog10() + 1;
+            match len % 2 {
+                0 => {
+                    new_stones.push(stone.div_euclid(10u64.pow(len/2)));
+                    new_stones.push(stone.rem_euclid(10u64.pow(len/2)));
+                },
+                1 => new_stones.push(stone * 2024),
+                _ => {}
+            }
+        });
+        stones = new_stones.clone();
+        new_stones.clear();
+    }
+    Some(stones.len() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    create_stones::<75>(input)
+    let stones: Vec<u64> = input.split_ascii_whitespace().map(|stone| stone.parse().unwrap()).collect_vec();
+    // let mut new_stones: Vec<u64> = vec![];
+
+    let sum_stones: Option<u32> = stones.iter().map(|&stone| {
+        let mut stones = vec![stone];
+        create_stone_alt(&mut stones)
+    }).sum();
+    sum_stones
 }
 
 #[cfg(test)]
